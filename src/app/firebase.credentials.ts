bfa0cae677b5bce1347a02a7ac81172550052d64
Johnnyboy <joaomellocorrea@gmail.com>
2018-01-02 19:00:40 -0200

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,10 +1,10 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDeAEFfgcjxCCYs3Tio-ZlE2DqTdRJGCJ0",
-    authDomain: "ionicshpppinglist.firebaseapp.com",
-    databaseURL: "https://ionicshpppinglist.firebaseio.com",
-    projectId: "ionicshpppinglist",
-    storageBucket: "ionicshpppinglist.appspot.com",
-    messagingSenderId: "874929996897"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "..."
 
 
 };
